6b6d379ced4fb168c57ebc21242d978f0059e8a1
Samu <samuelsabino987@gmail.com>
2024-10-31 17:07:20 -0300

    Update 2-4.c

--- 2 lista/2-4.c
+++ 2 lista/2-4.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 void calculaIMC(float peso, int altura){
-    float IMC;
-    IMC =peso/((altura*altura)/10000);
+    float IMC, altf;
+    altf = altura/100;
+    IMC =peso/((altf*altf));
     printf("O IMC e %.2f\n", IMC);
 }
 int main(){
-    float peso, altura;
+    float peso;
+    int alt;
     printf("Digite o peso em kilogramas\n");
     scanf("%f", &peso);
     printf("Digite a altura em centimetros\n");
-    scanf("%d", &altura);
+    scanf("%d", &alt);
     calculaIMC(peso, altura);
     return 0;
 }
